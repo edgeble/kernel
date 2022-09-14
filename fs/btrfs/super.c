2620be36d4508a8f0f9c0ad5bf1ab92944ca320f
Josef Bacik <josef@toxicpanda.com>
2022-09-14 11:06:38 -0400

    btrfs: move btrfs_path_cachep out of ctree.h
    This is local to the ctree code, remove it from ctree.h and inode.c,
    create new init/exit functions for the cachep, and move it locally to
    ctree.c.
    
    Reviewed-by: Qu Wenruo <wqu@suse.com>
    Reviewed-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>
    Reviewed-by: David Sterba <dsterba@suse.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -2727,9 +2727,13 @@ static int __init init_btrfs_fs(void)
 	if (err)
 		goto free_cachep;
 
-	err = extent_state_init_cachep();
+	err = btrfs_ctree_init();
 	if (err)
 		goto free_transaction;
+
+	err = extent_state_init_cachep();
+	if (err)
+		goto free_ctree;
 
 	err = extent_buffer_init_cachep();
 	if (err)
@@ -2799,6 +2803,8 @@ free_eb_cachep:
 	extent_buffer_free_cachep();
 free_extent_cachep:
 	extent_state_free_cachep();
+free_ctree:
+	btrfs_ctree_exit();
 free_transaction:
 	btrfs_transaction_exit();
 free_cachep:
@@ -2812,6 +2818,7 @@ free_compress:
 
 static void __exit exit_btrfs_fs(void)
 {
+	btrfs_ctree_exit();
 	btrfs_transaction_exit();
 	btrfs_destroy_cachep();
 	btrfs_delayed_ref_exit();
